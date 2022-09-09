8250fe5c4495c65d5b8de96cd27edf9cd8453aac
kanelabs <ryan@kanelabs.com>
2022-09-09 09:31:38 -0500

    custom fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,6 @@ import { AppProps } from "next/app";
 import Head from "next/head";
 import { FC } from "react";
 import { ContextProvider } from "../contexts/ContextProvider";
-import { AppBar } from "../components/AppBar";
-import { ContentContainer } from "../components/ContentContainer";
-import { Footer } from "../components/Footer";
-import Notifications from "../components/Notification";
-import { ToastContainer } from "react-toastify";
 import { getLayout } from "components/layouts/SiteLayout";
 
 require("@solana/wallet-adapter-react-ui/styles.css");
