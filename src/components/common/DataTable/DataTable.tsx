37fb411f323fbc49797850fde03d0a85b1fa2e47
gokhanmelma <gokhanmelma@gmail.com>
2022-11-17 12:15:09 -0500

    remove table bottom border

--- src/components/common/DataTable/DataTable.tsx
+++ src/components/common/DataTable/DataTable.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export const DataTable = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="w-full overflow-x-hidden border bg-[#F8F9F9] dark:bg-[#393939]  dark:border-[#626262] rounded-lg">
+    <div className="w-full overflow-x-hidden border border-b-0 bg-[#F8F9F9] dark:bg-[#393939]  dark:border-[#626262] rounded-lg">
       <div className="table-auto w-full">
         <div className="w-full">{children}</div>
       </div>
