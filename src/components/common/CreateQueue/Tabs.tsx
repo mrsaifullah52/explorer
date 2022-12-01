83c41a2dc925ca1056d1a57747ecebfb0075942c
gokhanmelma <gokhanmelma@gmail.com>
2022-12-01 15:39:22 -0500

    add tooltip

--- src/components/common/CreateQueue/Tabs.tsx
+++ src/components/common/CreateQueue/Tabs.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactTooltip from "react-tooltip";
 
 const threadTitles: string[] = [
   "Hello world",
@@ -19,18 +20,22 @@ export default function Tabs({ activeTab, setActiveTab }: TabsType) {
       {threadTitles.map((title: string, index: number) => {
         return (
           <button
-            key={`${title}_${index}`}
+            key={`button_${title}_${index}`}
             onClick={() => setActiveTab(index)}
             className={`btn px-3 py-1 mr-1 my-1 ${
               activeTab === index
                 ? "border border-gray-900 bg-gray-900 text-gray-100 dark:bg-gray-900"
                 : "border border-gray-900 dark:border-gray-500 dark:bg-gray-500"
             }`}
+            data-html={true}
+            data-tip={index !== 0 ? "Coming soon!" : ""}
+            // disabled={index !== 0}
           >
             {title}
           </button>
         );
       })}
+      <ReactTooltip />
     </div>
   );
 }
