ac7dcfd77ce63b8746a8ba045eff586c1a54cc30
kanelabs <ryan@kanelabs.com>
2022-12-15 08:58:35 -0600

    styling

--- src/components/common/AccountRenderer/index.tsx
+++ src/components/common/AccountRenderer/index.tsx
@@ -3,10 +3,9 @@ import { PublicKey } from "@solana/web3.js";
 import { useSearch } from "contexts/SearchContext";
 import { useSolana } from "contexts/SolanaContext";
 import { FC, useMemo } from "react";
-import { getExplorerAccountLink, tryIntoPubkey, tryIsBuffer } from "@clockwork-xyz/sdk";
+import { getExplorerAccountLink, tryIntoPubkey, tryIsBuffer, toSentenceCase } from "@clockwork-xyz/sdk";
 import { DataTable, DataTableRow, DataTableRowExpandable } from "../DataTable";
 import { DataTableRowAccount } from "../DataTable/DataTableRowAccount";
-import { toSentenceCase } from "@clockwork-xyz/sdk";
 import { DataTableRowBuffer } from "../DataTable/DataTableRowBuffer";
 import { AccountTableTitle } from "../AccountTableTitle";
 
@@ -16,6 +15,7 @@ export const AccountRenderer = () => {
   if (!data)
     return <div className="py-6 rounded-lg flex flex-col space-y-6"></div>;
 
+  console.log("data", data);
   return (
     <div className="py-6 rounded-lg flex flex-col mb-6">
       <AccountTableTitle accountType={data.accountType} />
@@ -29,6 +29,124 @@ export const AccountRenderer = () => {
   );
 };
 
+const mapEntriesToComponents = (entries: [string, any][], depth = 0, clusterNetwork) => {
+  return entries.map(([name, value], i) => {
+    const label = toSentenceCase(name);
+
+    if (typeof value === "boolean") {
+      return (
+        <DataTableRow
+          key={`${name}_${i}_${depth}`}
+          label={label}
+          depth={depth}
+          value={value ? "true" : "false"}
+        />
+      );
+    }
+
+    if (typeof value === "string") {
+      return (
+        <DataTableRow
+          key={`${name}_${i}_${depth}`}
+          label={label}
+          depth={depth}
+          value={value}
+        />
+      );
+    }
+
+    if (typeof value === "number") {
+      return (
+        <DataTableRow
+          key={`${name}_${i}_${depth}`}
+          label={label}
+          depth={depth}
+          value={value}
+        />
+      );
+    }
+
+    if (value instanceof web3.PublicKey || value instanceof PublicKey) {
+      const link = getExplorerAccountLink(value, clusterNetwork);
+      return (
+        <DataTableRow
+          key={`${name}_${i}_${depth}`}
+          label={label}
+          depth={depth}
+          value={value.toBase58()}
+          link={link}
+        />
+      );
+    }
+
+    if (value instanceof BN) {
+      return (
+        <DataTableRow
+          key={`${name}_${i}_${depth}`}
+          label={label}
+          depth={depth}
+          value={value.toString()}
+          fontMono
+        />
+      );
+    }
+
+    if (tryIsBuffer(value)) {
+      return (
+        <DataTableRowBuffer
+          key={`${name}_${i}_${depth}`}
+          label={label}
+          depth={depth}
+          value={value}
+        />
+      );
+    }
+
+    if (typeof value === "object") {
+      if (name === "accounts") {
+        return (
+          <>
+            {Object.entries(value).map(([index, accountIx]) => {
+              const { pubkey, isWritable, isSigner } = accountIx as {
+                pubkey: PublicKey;
+                isWritable: boolean;
+                isSigner: boolean;
+              };
+              return (
+                <DataTableRowAccount
+                  key={name + index}
+                  link={getExplorerAccountLink(pubkey, clusterNetwork)}
+                  label={`Account #${parseInt(index) + 1}`}
+                  pubkey={pubkey}
+                  isSigner={isSigner}
+                  isWritable={isWritable}
+                  depth={depth}
+                />
+              );
+            })}
+          </>
+        );
+      }
+
+      try {
+        const children = mapEntriesToComponents(
+          Object.entries(value),
+          depth + 1,
+          clusterNetwork
+        );
+        return (
+          <DataTableRowExpandable label={label} depth={depth}>
+            {children}
+          </DataTableRowExpandable>
+        );
+      } catch (error) {
+        console.error(value);
+      }
+    }
+  });
+};
+
+
 export const RecursiveAccountRenderer: FC<{
   account: Record<any, any>;
   address: string;
@@ -36,123 +154,12 @@ export const RecursiveAccountRenderer: FC<{
   const { cluster } = useSolana();
   let entries = useMemo(() => Object.entries(account), [account]);
 
-  const mapEntriesToComponents = (entries: [string, any][], depth = 0) => {
-    return entries.map(([name, value], i) => {
-      const label = toSentenceCase(name);
-
-      if (typeof value === "boolean") {
-        return (
-          <DataTableRow
-            key={`${name}_${i}_${depth}`}
-            label={label}
-            depth={depth}
-            value={value ? "true" : "false"}
-          />
-        );
-      }
-
-      if (typeof value === "string") {
-        return (
-          <DataTableRow
-            key={`${name}_${i}_${depth}`}
-            label={label}
-            depth={depth}
-            value={value}
-          />
-        );
-      }
-
-      if (typeof value === "number") {
-        return (
-          <DataTableRow
-            key={`${name}_${i}_${depth}`}
-            label={label}
-            depth={depth}
-            value={value}
-          />
-        );
-      }
-
-      if (value instanceof web3.PublicKey || value instanceof PublicKey) {
-        const link = getExplorerAccountLink(value, cluster.network);
-        return (
-          <DataTableRow
-            key={`${name}_${i}_${depth}`}
-            label={label}
-            depth={depth}
-            value={value.toBase58()}
-            link={link}
-          />
-        );
-      }
-
-      if (value instanceof BN) {
-        return (
-          <DataTableRow
-            key={`${name}_${i}_${depth}`}
-            label={label}
-            depth={depth}
-            value={value.toString()}
-            fontMono
-          />
-        );
-      }
-
-      if (tryIsBuffer(value)) {
-        return (
-          <DataTableRowBuffer
-            key={`${name}_${i}_${depth}`}
-            label={label}
-            depth={depth}
-            value={value}
-          />
-        );
-      }
-
-      if (typeof value === "object") {
-        if (name === "accounts") {
-          return (
-            <>
-              {Object.entries(value).map(([index, accountIx]) => {
-                const { pubkey, isWritable, isSigner } = accountIx as {
-                  pubkey: PublicKey;
-                  isWritable: boolean;
-                  isSigner: boolean;
-                };
-                return (
-                  <DataTableRowAccount
-                    key={name + index}
-                    link={getExplorerAccountLink(pubkey, cluster.network)}
-                    label={`Account #${parseInt(index) + 1}`}
-                    pubkey={pubkey}
-                    isSigner={isSigner}
-                    isWritable={isWritable}
-                    depth={depth}
-                  />
-                );
-              })}
-            </>
-          );
-        }
-
-        try {
-          const children = mapEntriesToComponents(
-            Object.entries(value),
-            depth + 1
-          );
-          return (
-            <DataTableRowExpandable label={label} depth={depth}>
-              {children}
-            </DataTableRowExpandable>
-          );
-        } catch (error) {
-          console.error(value);
-        }
-      }
-    });
-  };
 
   const addressPubkey = useMemo(() => tryIntoPubkey(address), [address]);
+
+  const components = useMemo(() => {
+    return mapEntriesToComponents(entries, 0, cluster.network);
+  }, [entries, cluster.network]);
 
   return (
     <>
@@ -165,7 +172,7 @@ export const RecursiveAccountRenderer: FC<{
           link={getExplorerAccountLink(addressPubkey, cluster.network)}
         />
       )}
-      {mapEntriesToComponents(entries, 0)}
+      {components}
     </>
   );
 };
--- src/pages/address/[address].tsx
+++ src/pages/address/[address].tsx
@@ -9,59 +9,139 @@ import { useAddressSignatures } from "hooks/useAddressSignatures";
 
 import { AccountTableTitle } from "components/common/AccountTableTitle";
 import { Loader } from "components/common/Loader";
+import { CopyButton } from "components/common/CopyButton";
+import Link from "next/link";
+import { getExplorerBlockLink, getExplorerLink } from "@clockwork-xyz/sdk";
+import { useSolana } from "contexts/SolanaContext";
+import { format, formatDistanceToNowStrict } from "date-fns";
 
 // Write a function to use the getSignaturesForAddress RPC endpoint to display a table of the recent transactions an account has been included in. This table should be displayed on all account templates in the /address/* path. https://docs.solana.com/developing/clients/jsonrpc-api#getsignaturesforaddress
 
 export const AddressSignaturesTable = () => {
   const router = useRouter();
+  const { cluster, customEndpoint } = useSolana();
   const { address } = router.query;
   const { data, error, loading, reset } = useAddressSignatures(
     address as string
   );
 
-  console.log("data", data);
+  console.log({ data, error, loading, reset });
   return (
     <div className="py-6 rounded-lg flex flex-col mb-6">
       <AccountTableTitle accountType={"Transaction History"} />
-      <DataTable>
-        <div className="flex">
-          <div className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-            Signature
-          </div>
-          <div className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-            Block
-          </div>
-          <div className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-            Age
-          </div>
-          <div className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-            Timestamp
-          </div>
-          <div className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-            Result
-          </div>
-        </div>
-        {data?.map((signature) => {
-          return (
-            <div className="" key={signature.signature}>
-              <div className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                {signature.signature}
-              </div>
+      <div className="bg-[#F8F9F9] dark:bg-[#393939]  dark:border-[#626262] rounded-lg">
+        <div className="overflow-x-scroll">
+          <table className="table-auto border-collapse">
+            <thead className="text-sm text-[#0E1114] dark:text-[#979797]">
+              <tr>
+                <td className="p-4 whitespace-nowrap text-sm">Signature</td>
+                <td className="p-4 whitespace-nowrap text-sm">Block</td>
+                <td className="p-4 whitespace-nowrap text-sm">Age</td>
+                <td className="p-4 whitespace-nowrap text-sm">Timestamp</td>
+                <td className="p-4 whitespace-nowrap text-sm">Result</td>
+              </tr>
+            </thead>
+            <tbody className="text-[13px] text-[#0E1114] dark:text-white">
+              {data?.map((signatureInfo) => {
+                return (
+                  <tr
+                    className="border-t border-b dark:border-[#626262] min-w-full"
+                    key={signatureInfo.signature}
+                  >
+                    <td className="whitespace-nowrap p-0">
+                      <span
+                        // rel="noopener noreferrer"
+                        className={`p-4 whitespace-nowrap flex justify-end items-center space-x-2`}
+                      >
+                        <CopyButton valueToCopy={signatureInfo.signature} />
+                        <Link
+                          href={getExplorerLink(
+                            signatureInfo.signature,
+                            cluster.network
+                          )}
+                        >
+                          <a>
+                            <div className="flex items-center space-x-2">
+                              <p className="space-x-2   hover:underline font-light font-['IBM_Plex_Mono']">
+                                {signatureInfo.signature.slice(0, 45) + "..."}
+                              </p>
+                              {/* <ArrowUpIcon className="h-4 w-4 cursor-pointer rotate-45" /> */}
+                            </div>
+                          </a>
+                        </Link>
+                      </span>
+                    </td>
+                    <td className="whitespace-nowrap p-0">
+                      <span
+                        // rel="noopener noreferrer"
+                        className={`p-4 whitespace-nowrap flex justify-end items-center space-x-2`}
+                      >
+                        <CopyButton valueToCopy={signatureInfo.slot} />
+                        <Link
+                          href={getExplorerBlockLink(
+                            signatureInfo.slot,
+                            cluster.network
+                          )}
+                        >
+                          <a>
+                            <div className="flex items-center space-x-2">
+                              <p className="space-x-2   hover:underline font-light font-['IBM_Plex_Mono']">
+                                {formatSlotNumber(signatureInfo.slot)}
+                              </p>
+                              {/* <ArrowUpIcon className="h-4 w-4 cursor-pointer rotate-45" /> */}
+                            </div>
+                          </a>
+                        </Link>
+                      </span>
+                    </td>
+                    <td className="p-4 whitespace-nowrap">
+                      <p className="space-x-2 font-light font-['IBM_Plex_Mono'] dark:text-[#979797]">
+                        {formatDistanceToNowStrict(
+                          new Date(signatureInfo.blockTime * 1000)
+                        )}
+                      </p>
+                    </td>
+                    <td className="p-4 whitespace-nowrap">
+                      <p className="space-x-2 font-light font-['IBM_Plex_Mono'] dark:text-[#979797]">
+                        {formatTimestamp(signatureInfo.blockTime)}
+                      </p>
+                    </td>
+                    <td className="p-4 whitespace-nowrap">
+                      <p className="space-x-2 font-light font-['IBM_Plex_Mono'] dark:text-[#979797]">
+                        {signatureInfo.err ? "Failed" : "Success"}
+                      </p>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+          {!loading && (
+            <div className="flex self-center items-center w-full justify-center px-6 py-4">
+              <span className="whitespace-nowrap w-full text-sm text-center justify-self-center">
+                {data && data.length === 0
+                  ? "No transactions found"
+                  : "Fetched full history"}
+              </span>
             </div>
-          );
-        })}
-
-        {data && data.length === 0 && (
-          <div className="flex self-center items-center w-full justify-center px-6 py-4">
-            <span className="whitespace-nowrap text-sm text-gray-500 text-center justify-self-center">
-              No transactions found
-            </span>
-          </div>
-        )}
-      </DataTable>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
+
+const formatTimestamp = (unixTimestamp: number) => {
+  const date = new Date();
+
+  return format(unixTimestamp * 1000, "MMM d, yyyy HH:mm:ss xxxx");
+};
+
+// adds commas to every 3 numbers for readability
+const formatSlotNumber = (num: number) => {
+  return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+};
+
 
 const AddressPage = () => {
   const router = useRouter();
